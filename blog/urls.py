f844dbe2a7d11721e7e066dd3afaa8fa7aebe05b
unknown <you@example.com>
2020-12-05 18:10:44 +0300

    added imports in URL file

--- blog/urls.py
+++ blog/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from main.urls import views
+from django.conf.urls.static import static
+from . import settings
+
+
 
 
 
